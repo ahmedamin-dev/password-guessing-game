5d3ac4c596306ef8f3ea3d42a13eaa6626da8b9c
Ahmed <ahmed@jsmastery.pro>
2025-12-24 12:17:43 +0200

    added a delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useEffect, useState, type ChangeEvent } from "react";
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const password = "1234";
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, "SHOW PASS", 0, "DELETE"];
+const generatePassword = () => {
+  let pass = "";
+  for (let i = 0; i < 4; i++) {
+    const randomNum = Math.floor(Math.random() * 10);
+    pass += String(randomNum);
+  }
+
+  return pass;
+};
 
 const App = () => {
   const [inputPassword, setInputPassword] = useState<string>("");
+  const [password, setPassword] = useState(() => generatePassword());
   const [correct, setCorrect] = useState<boolean | null>(null);
   const [error, setError] = useState<boolean>(false);
 
-  const handleClick = (num: number) => {
-    setInputPassword((prev) => (prev += String(num)));
+  console.log(password);
+
+  const handleClick = (num: number | string) => {
+    if (typeof num === "number") {
+      setInputPassword((prev) => (prev += String(num)));
+    } else {
+      setInputPassword((prev) => prev.slice(0, -1));
+    }
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -19,6 +34,7 @@ const App = () => {
   const handleReset = () => {
     setError(false);
     setCorrect(null);
+    setPassword(generatePassword());
   };
 
   useEffect(() => {
@@ -36,11 +52,11 @@ const App = () => {
     };
 
     handleGameState();
-  }, [inputPassword]);
+  }, [inputPassword, password]);
 
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <section className="flex flex-col items-center gap-8">
+      <section className="flex flex-col items-center gap-5">
         {!correct && (
           <>
             <h1 className="text-4xl">Try to guess the 4 digit Password</h1>
@@ -51,15 +67,14 @@ const App = () => {
               onChange={handleChange}
               className="border text-center px-4 py-1 outline-none rounded-md"
             />
+            <div>{password}</div>
             <div className="grid grid-cols-3 gap-4">
               {arr.map((num, index) => (
                 <button
                   key={index}
                   disabled={error}
                   onClick={() => handleClick(num)}
-                  className={`border px-4 py-2 rounded-full hover:bg-gray-50/10 transition-colors cursor-pointer disabled:bg-gray-50/30 ${
-                    num === 0 && "col-start-2"
-                  }`}
+                  className="border text-sm px-4 py-2 rounded-full hover:bg-gray-50/10 transition-colors cursor-pointer disabled:bg-gray-50/30"
                 >
                   {num}
                 </button>
